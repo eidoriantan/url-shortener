964ba84f83938bac86aa6266325f9d8db897e828
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-06-09 14:52:08 +0800

    perf: use React `build` output

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/', express.static('public'))
+app.use('/', express.static('build'))
 app.use('/api/shorts', dbMiddleware(client), shorts)
 app.use('/r/', dbMiddleware(client), redirect)
 
